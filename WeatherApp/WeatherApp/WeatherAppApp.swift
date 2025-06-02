6215327e65d45c2f2b7020628187ab6c236c5dd6
Dmitriy <skokdmitriy@gmail.com>
2025-06-02 14:52:48 +0700

    "Initial Xcode project setup"

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Дмитрий Скок on 02.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
